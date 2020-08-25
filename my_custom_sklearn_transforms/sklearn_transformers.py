f5db94112a7a07eac7097a52323c0b74a2faf407
Enmanuel Magallanes <fmagalla@fiec.espol.edu.ec>
2020-08-25 10:47:13 -0500

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,5 +1,5 @@
 from sklearn.base import BaseEstimator, TransformerMixin
-import pandas as pd
+
 
 # All sklearn Transforms must have the `transform` and `fit` methods
 class DropColumns(BaseEstimator, TransformerMixin):
@@ -10,6 +10,7 @@ class DropColumns(BaseEstimator, TransformerMixin):
         return self
     
     def transform(self, X):
+        import pandas as pd
         # Primero copiamos el dataframe de datos de entrada 'X'
         data = X.copy()
         # Devolvemos un nuevo dataframe de datos sin las columnas no deseadas
